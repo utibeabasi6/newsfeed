62df88e5fffc1ac063134ad4d2f94318137082e2
utibeabasi6 <utibeabasiumanah6@gmail.com>
2021-10-18 15:03:36 +0100

    update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt install python3-pip -y
 pip3 install -r /home/ubuntu/newsfeed/requirements.txt
-echo "Hello world" >> /home/ubuntu/newsfeed
+cd /home/ubuntu/newsfeed && echo "Hello world" >> hello.txt
